e57796c885937da2041a3127643bc77665d7091c
Prayuda Sagita Wardana <wpruda@gmail.com>
2022-06-12 12:09:25 +0700

    Tugas 7 promise

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    let theater1 = await promiseTheaterIXX();
+    let theater2 = await promiseTheaterVGC();
+    let hasil = (await theater1).concat(await theater2);
+    let emosi = 0;
+    hasil.map((theater1) => theater1.hasil === emosi && (emosi += 1));
+    return emosi;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
